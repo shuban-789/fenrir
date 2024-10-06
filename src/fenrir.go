2905ecc56a7b4aed1fb913be4d617db301688462
shuban-789 <cybergeek11929@gmail.com>
2024-10-06 13:47:08 -0700

    make clean is working

--- src/fenrir.go
+++ src/fenrir.go
@@ -8,39 +8,44 @@ import (
 	"io"
 )
 
-//////////////////////////////////////////////////////
-// SHA-256 checksum verification function for files //
-//////////////////////////////////////////////////////
+// SHA-256 checksum verification function for files
 func checksum(filePath string) string {
 	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Printf("ERROR OPENING FILE (\033[0;36m%s\033[0m): %s\n", filePath, err)
+		fmt.Printf("\033[31m[FAIL]\033[0m Error reading file (\033[0;36m%s\033[0m): %s\n", filePath, err)
 		return ""
 	}
 	defer file.Close()
 
 	hash := sha256.New()
 	if _, err := io.Copy(hash, file); err != nil {
-		fmt.Printf("ERROR WITH HASH GENERATION (\033[0;36m%s\033[0m): %s\n", filePath, err)
+		fmt.Printf("\033[31m[FAIL]\033[0m Error retrieving sha256 hash (\033[0;36m%s\033[0m): %s\n", filePath, err)
 		return ""
 	}
 
 	return fmt.Sprintf("%x", hash.Sum(nil))
 }
 
-///////////////////////////////////////////////////////////
-// O(log(n)) Complexity binary search-based verification //
-///////////////////////////////////////////////////////////
+func log_check(content string, logfile string) {
+	byte_content := []byte(content)
+    err := os.WriteFile(logfile, byte_content, 0777)
+    if err != nil {
+        fmt.Printf("\033[31m[FAIL]\033[0m Error appending content to logfile (\033[0;36m%s\033[0m): %s\n", logfile, err)
+		return
+    }
+}
+
+// O(log(n)) Complexity binary search-based verification
 func binary_search_verification(base string, target string) {
 	basefiles, err := os.ReadDir(base)
 	if err != nil {
-		fmt.Printf("ERROR READING DIRECTORY (%s): %s\n", base, err)
+		fmt.Printf("\033[31m[FAIL]\033[0m Error reading directory (%s): %s\n", base, err)
 		return
 	}
 
 	targetfiles, err := os.ReadDir(target)
 	if err != nil {
-		fmt.Printf("ERROR READING DIRECTORY (%s): %s\n", target, err)
+		fmt.Printf("\033[31m[FAIL]\033[0m Error reading directory (%s): %s\n", target, err)
 		return
 	}
 
@@ -69,7 +74,7 @@ func binary_search_verification(base string, target string) {
 				if currentChecksum == targetChecksum {
 					fmt.Printf("\033[32m[OK]\033[0m File matched (\033[0;36m%s/%s\033[0m --> \033[0;36m%s/%s\033[0m)\n", base, currentFileName, target, targetFilenames[mid])
 				} else {
-					fmt.Printf("\033[31m[ALERT]\033[0m Checksum mismatch (\033[0;36m%s/%s\033[0m)\n", base, currentFileName)
+					fmt.Printf("\033[31m[ALERT]\033[0m Checksum conflict (\033[0;36m%s/%s\033[0m)\n", base, currentFileName)
 				}
 				found = true
 				break
@@ -119,9 +124,7 @@ func binary_search_verification(base string, target string) {
 	}
 }
 
-/////////////////////////
-// Help and usage menu //
-/////////////////////////
+// Help and usage menu
 func help() {
 	// Usage
 	fmt.Printf("Usage: ./fenrir [OPTION1] [ARGUMENT1] ... [OPTIONn] [ARGUMENTn]\n")
@@ -136,15 +139,18 @@ func help() {
 	fmt.Printf("	./fenrir -b ./simulation/base_dir -t ./simulation/target_dir\n")
 }
 
-//////////////////////////////////////
-// Main function and argument logic //
-//////////////////////////////////////
+// Main function and argument logic
 func main() {
 	if len(os.Args) > 1 {
 		if strings.Compare(os.Args[1], "-b") == 0 {
 			if len(os.Args) >= 5 && strings.Compare(os.Args[3], "-t") == 0 {
 				base := os.Args[2]
 				target := os.Args[4]
+				conflict_log, conflict_log_err := os.Create("conflicts.log")
+				if conflict_log_err != nil {
+					fmt.Printf("\033[31m[FAIL]\033[0m Error creating log file (\033[0;36mconflicts.log\033[0m): %s\n", err)
+					return
+				}
 				binary_search_verification(base, target)
 			} else {
 				help()
